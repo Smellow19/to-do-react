11f170a1e9a2e5d7f796f4be22b5343ae9615d61
Tyler Bridges <tylerbridges@Tylers-MacBook-Pro.local>
2017-12-08 18:49:24 -0600

    Need to add Delete button

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,11 +4,13 @@ class ToDo extends Component {
   render(){
     return(
       <li>
-      <input type="checkbox" checked={ this.props.isCompleted } />
+      <input type="checkbox" checked={ this.props.isCompleted } onChange= {this.props.toggleComplete} />
        <span>{ this.props.description }</span>
+       <button>Trash</button>
       </li>
     );
   }
 }
+
 
 export default ToDo;
